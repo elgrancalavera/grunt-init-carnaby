f302d35209ecd30c531305d50bda06fd3276ec3b
Leon Coto <leon.coto@mcsaatchi.com>
2013-08-12 18:20:57 +0100

    Initial work on template files.

---
+++ template.js
@@ -0,0 +1,9 @@
+/*
+ * grunt-init-carnaby
+ * https://github.com/elgrancalavera/grunt-init-carnaby
+ *
+ * Copyright (c) 2013 M&CSaatchi
+ * Licensed under the MIT license.
+ */
+'use-strict';
+exports = require('./lib/carnaby');
